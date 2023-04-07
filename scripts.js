ed0bb76c1e36465a105398a3bedfa38cc93563d4
Kavix0 <kavix0@gmail.com>
2023-04-07 02:16:35 -0500

    Added basic math functions

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ e.addEventListener('mousedown', parseInput(e))
 
 function parseInput(e){
 }
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return (Number(num2) === 0) ? 'Division by 0' : (num1 / num2);
+}
