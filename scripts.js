a91f65def52c2dac68db7aeea2204fd45b5d9116
Kavix0 <kavix0@gmail.com>
2023-04-07 02:48:18 -0500

    Added operate function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
 let currentInput = [];
 const buttons = [...document.getElementsByClassName('gridItem')];
+let lOperand = 0;
+let operator = '';
+let rOperand = 0;
+
 
 buttons.forEach((e) => {
 e.addEventListener('mousedown', parseInput(e))
 });
 
 function parseInput(e){
+}
+
+function operate(num1, sign, num2)
+{
+    switch(sign){
+        case '+' : 
+            return add(num1,num2);
+        case '-' :
+            return subtract(num1,num2);
+        case '*' :
+            return multiply(num1,num2);
+        case '/' :
+            return divide(num1,num2);
+    }
 }
 
 function add(num1, num2){
