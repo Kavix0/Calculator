f1caa9f7a88ca191721c1ec6cf4f55133d3eb34d
Kavix0 <kavix0@gmail.com>
2023-04-11 00:59:03 -0500

    Select all buttons and add eventListeners

--- scripts.js
+++ scripts.js
@@ -1,15 +1,31 @@
-let currentInput = [];
-const buttons = [...document.getElementsByClassName('gridItem')];
+let currentInput = '';
 let lOperand = 0;
 let operator = '';
 let rOperand = 0;
 
+const numberButtons = [...document.querySelectorAll("[data-type = 'number']")];
+const operatorButtons = [...document.querySelectorAll("[data-type = 'operator']")];
+const equalsButton = document.querySelector("[data-type = 'equals']");
+const decimalButton = document.querySelector("[data-type = 'decimal'");
+const clearButton = document.querySelector("[data-type = 'clear'");
+const backButton = document.querySelector("[data-type = 'backspace'");
 
-buttons.forEach((e) => {
-e.addEventListener('mousedown', parseInput(e))
+numberButtons.forEach((e) => {
+e.addEventListener('click', appendNumber(e));
 });
 
-function parseInput(e){
+/*
+operatorButtons.forEach((e) => {
+    e.addEventListener('click', changeOperator);
+});
+
+equalsButton.addEventListener('click', calculateResult);
+decimalButton.addEventListener('click', appendDecimal);
+clearButton.addEventListener('click', clearScreen);
+backButton.addEventListener('click', backspace);
+*/
+
+function appendNumber(e){
 }
 
 function operate(num1, sign, num2)
@@ -23,6 +39,8 @@ function operate(num1, sign, num2)
             return multiply(num1,num2);
         case '/' :
             return divide(num1,num2);
+        case '%' :
+            return modulus(num1, num2);
     }
 }
 
@@ -40,4 +58,8 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return (Number(num2) === 0) ? 'Division by 0' : (num1 / num2);
+}
+
+function modulus(num1, num2){
+    return num1 % num2;
 }
